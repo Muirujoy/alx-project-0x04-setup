1805efaceecef653ac881c86f8384d39482516bb
Joy <muirujoybackup@gmail.com>
2025-07-31 19:54:07 +0300

    added

---
+++ alx-project-0x06/context/CountContext.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { createContext, useContext, useState } from "react";
+
+interface CountContextProps {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+const CountContext = createContext<CountContextProps | undefined>(undefined);
+
+export const CountProvider = ({ children }: { children: React.ReactNode }) => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+
+  return (
+    <CountContext.Provider value={{ count, increment, decrement }}>
+      {children}
+    </CountContext.Provider>
+  );
+};
+
+export const useCount = (): CountContextProps => {
+  const context = useContext(CountContext);
+  if (!context) {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+  return context;
+};
